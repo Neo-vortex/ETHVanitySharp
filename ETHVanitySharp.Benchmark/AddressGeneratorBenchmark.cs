88f5e461c707e856c25daad28d89fcd877075731
Mohamadreza Nakhleh <neo.vortex@pm.me>
2023-12-09 11:36:47 +0330

    (improve) make benchmark more realistic

--- ETHVanitySharp.Benchmark/AddressGeneratorBenchmark.cs
+++ ETHVanitySharp.Benchmark/AddressGeneratorBenchmark.cs
@@ -5,13 +5,19 @@ using Secp256k1Net;
 namespace ETHVanitySharp.Benchmark;
 
 [MemoryDiagnoser]
+[MemoryRandomization]
 public class AddressGeneratorBenchmark
 {
+    private Secp256k1 secp256k1;
+    [GlobalSetup]
+    public void Setup()
+    {
+          secp256k1 = new Secp256k1();
+    }
 
     [Benchmark]
     public void AddressGenerationAndValidation()
     {
-        var  secp256k1 = new Secp256k1();
         var candidatePublicAddress = string.Empty;
         var candidatePrivateKey = string.Empty;
         var randomBytes =  Utilities.Utilities.GetRandomBytes(32);
